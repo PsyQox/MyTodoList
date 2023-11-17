8ce533708493ad47ba407095db8d468df27a1eb9
PsyQox <pikithor@gmail.com>
2023-11-16 18:40:25 -0600

    client

--- client/src/components/Form/Form.tsx
+++ client/src/components/Form/Form.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { useDispatch} from "react-redux"
 import { addNotes } from "../../redux/actions"
 import { NoteT } from "../../types"
+import { validate } from "./validate"
 
 const Form = () =>{
     const dispatch = useDispatch()
@@ -13,20 +14,28 @@ const Form = () =>{
         priority: 0
     }
 
+
     const [form, setForm] = useState<NoteT>(initialFormState)
+    const [errors, setErrors] = useState<any>({})
 
     const onHandleSubmit = (event:React.FormEvent<HTMLFormElement>)=>{
         event.preventDefault()
-        dispatch(addNotes(form))
+        if (Object.keys(errors).length <= 0) {
+            if (!form.title || !form.priority || !form.date) {
+                alert("Missing data")
+            }else{
+                dispatch(addNotes(form))
+            }
+        }else{
+            alert('There are still errors.')
+        }
     }
 
     const onHandleChange = (event:React.ChangeEvent<HTMLInputElement|HTMLTextAreaElement>)=>{
         setForm({...form, [event.target.name]:event.target.value})
+        setErrors(validate({...form, [event.target.name]:event.target.value}))
     }
 
-    const validate = (data:any) => {
-        
-    }
 
     return(
         <div className="flex flex-wrap flex-col justify-center items-center">
@@ -34,13 +43,16 @@ const Form = () =>{
             <form className="flex flex-wrap flex-col justify-center items-center" onSubmit={onHandleSubmit} action="">
                 <label htmlFor="">Title:</label>
                 <input className="border-solid border-black border-2 rounded-lg w-60" onChange={onHandleChange} type="text" name="title" id="title" value={form.title}/>
+                {errors.title ? (<a className=" text-red-600">{errors.title}</a>):null}
                 <label htmlFor="">Description:</label>
                 <textarea className="border-solid border-black border-2 rounded-lg w-60" onChange={onHandleChange} name="description" id="description" cols={30} rows={2}></textarea>
                 <label htmlFor="">Date</label>
                 <input className="border-solid border-black border-2 rounded-lg w-60" onChange={onHandleChange} type="date" name="date" id="date" />
+                {errors.date ? (<a className=" text-red-600">{errors.date}</a>):null}
                 <label htmlFor="">Priority:</label>
                 <input className="border-solid border-black border-2 rounded-lg w-60" onChange={onHandleChange} type="number" name="priority" id="priority" />
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg mt-5" type="submit">Save</button>                              
+                {errors.priority ? (<a className=" text-red-600">{errors.priority}</a>):null}
+                { !form.title || !form.priority || !form.date ? (<button className=" bg-blue-300 font-bold py-2 px-4 rounded-lg  focus:outline-none mt-5 text-white" disabled> Save </button>):(<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg mt-5" type="submit">Save</button>)}
             </form>
             
         </div>
---
+++ client/src/components/Form/validate.ts
@@ -0,0 +1,31 @@
+import { NoteT } from "../../types"
+
+export const validate = (data:NoteT) => {
+    let incorrect:any = {}
+    
+    if (data.title) {
+        let title = data.title.trim()
+        if (!title) {
+            incorrect.title = "the title should not be empty"
+        } else if (title.length > 30) {
+            incorrect.title = "The title should be less than 30 characters"        
+        }    
+    }
+
+    if(data.date){
+        let date = data.date.trim()
+        if (!date) {
+            incorrect.date = "The date should not be empty"
+        }
+    }
+    
+    if(data.priority){
+        if (data.priority < 0) {
+            incorrect.priority = "The priority can not be less than 0"
+        }else if(data.priority > 10){
+            incorrect.priority = "The priority can not be more than 10"
+        }
+    }
+
+    return incorrect
+}
