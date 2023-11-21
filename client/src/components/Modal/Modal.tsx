58f837551b8d43df41fd66dea5f18c08c8ace655
PsyQox <pikithor@gmail.com>
2023-11-21 13:33:40 -0600

    modal

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -1,12 +1,23 @@
 import { useState } from "react"
 
-const Modal =()=>{
+const Modal =({isOpen, onClose, children}:any)=>{
 
-    const [isOpen, setIsOpen] = useState<boolean>(false);
+    // const [isOpen, setIsOpen] = useState<boolean>(false);
 
     return (
-        <>
-          <button className="bg-cyan-500 py-2 px-6 rounded-lg text-white font-bold m-5" onClick={()=> setIsOpen(true)}>Open modal</button>  
+        //backdrop
+        <div onClick={onClose} className={`fixed inset-0 flex justify-center items-center transition-colors ${ isOpen ? "visible bg-black/20":"invisible"}`}>
+          
+        
+          
+        </div>
+    )
+}
+
+export default Modal
+
+
+{/* <button className="bg-cyan-500 py-2 px-6 rounded-lg text-white font-bold m-5" onClick={()=> setIsOpen(true)}>Open modal</button>  
           {isOpen && (
             <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center ">
             <div className="bg-white p-5 rounded flex flex-col justify-center items-center gap-5">
@@ -19,11 +30,4 @@ const Modal =()=>{
             </div>
             
           </div>
-          )}
-          
-          
-        </>
-    )
-}
-
-export default Modal
+          )} */}
--- client/src/views/Home/Home.tsx
+++ client/src/views/Home/Home.tsx
@@ -1,13 +1,11 @@
 import Notes from "../../components/Notes/Notes"
 import Form from "../../components/Form/Form"
-import Modal from "../../components/Modal/Modal"
 
 const Home = ()=>{
 
     return(
         <div>
             <h1>I'm Home and show the notes!</h1>
-            <Modal />
             <Form />
             <Notes />
         </div>
