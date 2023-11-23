1758dc919d563160b593f34a8e27b6c7dc80a387
PsyQox <luisrodrizza@gmail.com>
2023-11-23 14:52:53 -0600

    modal

--- client/src/views/Home/Home.tsx
+++ client/src/views/Home/Home.tsx
@@ -1,12 +1,18 @@
 import Notes from "../../components/Notes/Notes"
 import Form from "../../components/Form/Form"
+import Modal from "../../components/Modal/Modal"
+import { useState } from "react"
 
 const Home = ()=>{
+const [isOpen, setIsOpen] = useState<boolean>(false);
 
     return(
         <div>
             <h1>I'm Home and show the notes!</h1>
-            <Form />
+            <button className="bg-red-500 text-white p-3 m-10 rounded-lg" onClick={()=> setIsOpen(true)}>Open</button>
+            <Modal isOpen={isOpen} onClose={()=>setIsOpen(false)}>
+                <Form />
+            </Modal>
             <Notes />
         </div>
     )
